df061388964a7c6ba22e8fdc5b90e9cd6088a5cc
Maxiaoteng <jizhuwo00@hotmail.com>
2018-09-05 20:36:14 +0800

    更新定时任务

--- baxi_git.sh
+++ baxi_git.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# 同步自身
+# 同步自身和定时任务
 cd ~/git_bash
 git pull gitlab master
 sleep 30s
 cp baxi_git.sh ~/
 chmod +x baxi_git.sh
+
 
 # 同步ifood
 cd ~/ifood
@@ -23,3 +24,9 @@ sleep 1m
 cd ~
 
 echo 'git 同步结束'
+
+
+# 定时任务更新
+crontab ~/git_bash/baxi_crontab.bk
+sudo service crontab restart
+echo '定时任务更新'
